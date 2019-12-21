315b90a2dd5dd2a315fbc0a8c9f94251c08adf45
kozake <s.kozale@gmail.com>
2019-12-21 23:17:58 +0900

    ラケットの操作

--- src/app/breakout/breakout.page.ts
+++ src/app/breakout/breakout.page.ts
@@ -39,6 +39,15 @@ export class BreakoutPage implements OnInit, AfterViewChecked {
   // ゲームステージ
   gameStage: PIXI.Container;
 
+  // タッチ回数
+  touchDownCount: number;
+
+  // 最後にタッチしたX座標
+  lastTouchDownX: number;
+
+  // ラケットの目指すX座標
+  racketTargetX: number;
+
   constructor(private zone: NgZone) {}
 
   ngOnInit() {
@@ -87,6 +96,29 @@ export class BreakoutPage implements OnInit, AfterViewChecked {
       }
     }
 
+    this.touchDownCount = 0;
+    this.lastTouchDownX = 0;
+    this.racketTargetX = this.racket.x;
+
+    // タッチ、ポインター、およびマウスイベントの有効化
+    this.gameStage.interactive = true;
+    this.gameStage
+      // タッチダウン
+      .on('pointerdown', (event: PIXI.interaction.InteractionEvent) =>
+        this.onTouchStart(event)
+      )
+      // タッチ終了
+      .on('pointerup', (event: PIXI.interaction.InteractionEvent) =>
+        this.onTouchEnd(event)
+      )
+      .on('pointerupoutside', (event: PIXI.interaction.InteractionEvent) =>
+        this.onTouchEnd(event)
+      )
+      // スワイプ
+      .on('pointermove', (event: PIXI.interaction.InteractionEvent) =>
+        this.onTouchMove(event)
+      );
+
     this.app.stage.addChild(this.gameStage);
   }
 
@@ -114,7 +146,63 @@ export class BreakoutPage implements OnInit, AfterViewChecked {
     }
   }
 
-  onFrame(delta: number) {}
+  onFrame(delta: number) {
+    this.moveRacket();
+    this.ball.x = this.racket.x + this.racket.width / 2 - this.ball.width / 2;
+  }
+
+  private moveRacket() {
+    const add = (this.racketTargetX - this.racket.x) / 10;
+    if (Math.abs(add) < 0.01) {
+      this.racket.x = this.racketTargetX;
+    } else {
+      this.racket.x += add;
+    }
+  }
+
+  /**
+   * タッチ開始
+   * @param event イベント
+   */
+  private onTouchStart(event: PIXI.interaction.InteractionEvent) {
+    this.touchDownCount++;
+    if (this.touchDownCount === 1) {
+      this.lastTouchDownX = event.data.global.x;
+      this.racketTargetX = this.racket.x;
+    }
+  }
+
+  /**
+   * タッチ終了
+   * @param event イベント
+   */
+  private onTouchEnd(event: PIXI.interaction.InteractionEvent) {
+    this.touchDownCount--;
+    if (this.touchDownCount <= 0) {
+      this.touchDownCount = 0;
+    }
+  }
+
+  /**
+   * スワイプ
+   * @param event イベント
+   */
+  private onTouchMove(event: PIXI.interaction.InteractionEvent) {
+    if (this.touchDownCount !== 1) {
+      return;
+    }
+    const x = event.data.global.x;
+    const distance = x - this.lastTouchDownX;
+
+    this.racketTargetX += distance / this.scale;
+    this.racketTargetX = Math.max(this.racketTargetX, 0);
+    this.racketTargetX = Math.min(
+      this.racketTargetX,
+      SCREEN_WIDTH - this.racket.width
+    );
+
+    this.lastTouchDownX = x;
+  }
 
   /**
    * 背景作成
